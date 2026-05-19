bfc619ac91b07c95293e60c31b989c844c1887e1
Ghadeer Ismael <ghadeerdevice.com>
2026-05-19 22:50:02 +0300

    Fix hero image responsive layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <main>
   {/* Hero Section */}
-<section className="w-full mt-16">
+<section className="w-full mt-16 md:mt-0">
   <img
     src="/Ready.png"
     alt="Ready to Flow"
